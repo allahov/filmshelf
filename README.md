7b19266d9fda98ac406419e00dafd27020c2f951
allahov <allahov@gmail.com>
2026-07-06 21:18:53 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# FilmShelf
+
+Personal movie tracker PWA.
+
+## What works
+
+- Add movie or TV series
+- Edit card
+- Delete card
+- Move cards between To Watch and Watched
+- Filter by type and genre
+- Save data in localStorage
+- Install as PWA on phone
+
+## Deploy
+
+Static project for GitHub + Vercel.
---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "filmshelf-cache-final-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : null)
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
